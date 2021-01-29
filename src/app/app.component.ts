ad7c6965eba8aa1c7e84f2817faab5e6bc0fce95
sarathiidss <sarathi@idsoftsource.com>
2021-01-29 17:04:02 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  inc: any = 0;
+  inc: any = 1;
   incs() {
     if (true) {
     } else {
